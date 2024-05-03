cc2de414c76042b10ea7b79c9d9aeeafd3e00b28
heyitsabd <abdahmad0603@gmail.com>
2024-05-03 19:57:15 +0530

    Navbar done

---
+++ src/Screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+     <NavBar/>
+     Body
+     <Footer/>
+    </div>
+  )
+}
